d5be858f7fc322105c62b0fcadb804cb28a0618f
tharadonth02 <tharadon.th02@gmail.com>
2025-11-11 22:32:23 +0700

    update11/11

--- indix.php
+++ indix.php
@@ -7,24 +7,28 @@
      <link rel="stylesheet" href="stlye.css">
 </head>
 <body>
-    <div class="from">
+    <div class="form">
      <form action="login" method="POST"> 
+        <br>
         <h1>เข้าสู่ระบบ</h1>
-            <img width=150px height=200px src="https://img.freepik.com/free-vector/bird-colorful-gradient-design-vector_343694-2506.jpg?semt=ais_hybrid&w=740&q=80" alt="">
-
-        <div>
+            <img width=150 height=200 src="https://img.freepik.com/free-vector/bird-colorful-gradient-design-vector_343694-2506.jpg?semt=ais_hybrid&w=740&q=80" alt="">
+        <div class="form_user">
+            <br>
             <label for="user_name">ชื่อผู้ใช้</label>
             <input type="text" id="user_name" name="user_name" required>
         </div>
 
-        <div>
+        <div class="form_pass">
             <label for="password">รหัสผ่าน</label>
             <input type="password" id="password" name="password" required>
         </div> <br>
 
 
-        <button type="submit" class="submit">ล็อกอิน</button>
-
-    </form> </div>
+        <button type="submit" class="submit">ล็อกอิน</button> <br><br>
+      <a href="login.php">สมัครสมาชิก</a>
+      <br><br>
+     </form> 
+    </div>
+   
 </body>
 </html>
--- login.php
+++ login.php
@@ -7,26 +7,40 @@
 </head>
 <body>
     <body>
-    <div class="from">
+    <link rel="stylesheet" href="css_login.css">    
+     
+    <div class="form">
      <form action="login" method="POST"> 
+        <br>
         <h1>สมัครสมาชิก</h1>
+        <br>
            
-        <div>
+        <div  class="form_12">
             <label for="user_name">ชื่อผู้ใช้</label>
             <input type="text" id="user_name" name="user_name" required>
 
-        </div>
+        </div><br>
 
-        <div>
+        <div class="form_12">
             <label for="password">รหัสผ่าน</label>
-            <input type="password" id="password" name="password" required>
+            <input type="password" id="password" name="password" required> 
+        </div><br>
+        <div class="form_12">
+            <label for="confirm_password">ยืนยันรหัสผ่าน</label>
+            <input type="password" id="confirm_password" name="confirm_password" required> 
+        </div><br>
+        <div class="form_12">
+            <label for="email">เมล</label>
+            <input type="email" id="email" name="email" required>
         </div> <br>
 
-
-        <button type="submit" class="submit">ล็อกอิน</button>
-
-    </form> </div>
+        <button type="submit" class="submit">ล็อกอิน</button> 
+        <br><br>
+            <a href="indix.php">ถ้ามี "บัญชีอยู่แล้ว" คลิก</a>
+            <br><br><br>
+    </form>
+ </div>
 </body>
-    <h1>8l=dadadadsa</h1>
+    
 </body>
 </html>
