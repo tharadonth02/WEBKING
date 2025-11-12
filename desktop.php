9d3c5786ce44c049c82440c2bb6d8abf1d0ab350
tharadonth02 <tharadon.th02@gmail.com>
2025-11-12 02:00:02 -0800

    update 12/11

---
+++ desktop.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="th">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ชื่อไรดี</title>
+    <link rel="stylesheet" href="css_desktop.css">
+</head>
+<body>
+
+<header>
+    <nav class="navbar">
+        <div class="logo">
+            <span class="logo-text">เว็ปของเฟรช</span>
+        </div>
+
+        <ul class="nav-links">
+            <li class="nav-item">เฟรช <span class="dropdown-arrow"></span></li>
+            <li class="nav-item">คิด<span class="dropdown-arrow"></span></li>
+            <li class="nav-item">ถึง <span class="dropdown-arrow"></span></li>
+            <li class="nav-item">เอิร์น <span class="dropdown-arrow"></span></li>
+        </ul>
+
+        <div class="search-container">
+            <input type="text" placeholder="Search..." class="search-input">
+            <button class="search-button">
+                &#9906; </button>
+        </div>
+
+        <div class="kebab-menu">
+            &#x22EE; </div>
+    </nav>
+</header>
+
+</body>
+</html>
